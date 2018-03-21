271a4f8a8d28be10b2482ae831a9cd86b0836421
Nelson Liu <nfliu@cs.washington.edu>
2018-03-20 22:57:34 -0700

    Add timestamping to wget, download GloVe vectors

--- download_data.sh
+++ download_data.sh
@@ -2,9 +2,12 @@ mkdir -p data
 cd data
 
 # Download train data
-wget https://s3-us-west-2.amazonaws.com/allennlp/datasets/academic-papers-example/train.jsonl
+wget -N https://s3-us-west-2.amazonaws.com/allennlp/datasets/academic-papers-example/train.jsonl
 
 # Download validation data
-wget https://s3-us-west-2.amazonaws.com/allennlp/datasets/academic-papers-example/dev.jsonl
+wget -N https://s3-us-west-2.amazonaws.com/allennlp/datasets/academic-papers-example/dev.jsonl
+
+# Download GloVe vectors
+wget -N https://s3-us-west-2.amazonaws.com/allennlp/datasets/glove/glove.6B.100d.txt.gz
 
 cd ..
